3e791c6f682d5e37c0e556ca497bc07820bcbc3c
hamedblue1381 <hamed.balanar@gmail.com>
2024-02-20 13:30:29 +0330

    refactor: moved json response and request

--- models/model.go
+++ models/model.go
@@ -13,3 +13,13 @@ type Soda struct {
 type Coffe struct {
 	Item
 }
+
+type VendingMachineRequest struct {
+	Count int `json:"count"`
+}
+type VendingMachineResponse struct {
+	Id    int    `json:"id"`
+	Stock int    `json:"stock"`
+	Coins int    `json:"coins"`
+	Items []Item `json:"items"`
+}
